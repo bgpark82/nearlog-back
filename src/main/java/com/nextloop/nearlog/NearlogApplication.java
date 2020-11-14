20a2d4f014aaeb2bb5b6d92f04e68ca82914bece
bgpark <bgpark82@gmail.com>
2020-11-14 23:53:15 +0900

    feat: .travis.yml 기본설정

---
+++ src/main/java/com/nextloop/nearlog/NearlogApplication.java
@@ -0,0 +1,13 @@
+package com.nextloop.nearlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NearlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NearlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nextloop/nearlog/NearlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nextloop.nearlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NearlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
